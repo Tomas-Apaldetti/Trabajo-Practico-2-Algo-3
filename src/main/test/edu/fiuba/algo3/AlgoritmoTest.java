584d60d765d70b9234fa86e10baad048ac225300
perti <aperticaro@fi.uba.ar>
2021-02-06 16:07:31 -0300

    Se agregaron tests para AlgoritmoTest

--- src/main/test/edu/fiuba/algo3/AlgoritmoTest.java
+++ src/main/test/edu/fiuba/algo3/AlgoritmoTest.java
@@ -73,4 +73,53 @@ public class AlgoritmoTest {
         assertFalse(personaje.obtenerPosicion().estaPintado());
     }
 
+    @Test
+    public void algoritmoConBloqueInvertirDentroDeBloqueDeRepeticionYLapizBajoInvertidoSeEjecutaCorrectamente()
+    {
+        Personaje personaje = new Personaje();
+        IAccionCompuesta algoritmo = new Algoritmo();
+        IAccion bloqueBajarLapiz = new BloqueBajarLapiz();
+        IAccion bloqueArriba = new BloqueArriba();
+        IAccion bloqueIzquierda = new BloqueIzquierda();
+        IAccionCompuesta bloqueRepetir = new BloqueRepetir(3);
+        IAccionCompuesta bloqueInvertir = new BloqueInvertir();
+
+        Posicion posicionEsperada = new Posicion(3, -3);
+
+        algoritmo.agregarAccion(bloqueBajarLapiz);
+        bloqueInvertir.agregarAccion(bloqueIzquierda);
+        bloqueInvertir.agregarAccion(bloqueArriba);
+        bloqueInvertir.agregarAccion(bloqueBajarLapiz);
+        bloqueRepetir.agregarAccion(bloqueInvertir);
+        algoritmo.agregarAccion(bloqueRepetir);
+        algoritmo.ejecutar(personaje);
+
+        assertEquals(posicionEsperada, personaje.obtenerPosicion());
+        assertFalse(personaje.obtenerPosicion().estaPintado());
+
+    }
+
+    @Test
+    public void algoritmoConBloqueRepetirDentroDeBloqueInvertirInvierteTodasLasAccionesDentroDelBloqueRepetir()
+    {
+        Personaje personaje = new Personaje();
+        IAccionCompuesta algoritmo = new Algoritmo();
+        IAccionCompuesta bloqueRepetir = new BloqueRepetir(3);
+        IAccionCompuesta bloqueInvertir = new BloqueInvertir();
+        IAccion bloqueAbajo = new BloqueAbajo();
+        IAccion bloqueDerecha = new BloqueDerecha();
+        IAccion bloqueSubirLapiz = new BloqueSubirLapiz();
+
+        Posicion posicionEsperada = new Posicion(-3, 3);
+
+        bloqueRepetir.agregarAccion(bloqueSubirLapiz);
+        bloqueRepetir.agregarAccion(bloqueDerecha);
+        bloqueRepetir.agregarAccion(bloqueAbajo);
+        bloqueInvertir.agregarAccion(bloqueRepetir);
+        algoritmo.agregarAccion(bloqueInvertir);
+        algoritmo.ejecutar(personaje);
+
+        assertEquals(posicionEsperada, personaje.obtenerPosicion());
+        assertTrue(personaje.obtenerPosicion().estaPintado());
+    }
 }
