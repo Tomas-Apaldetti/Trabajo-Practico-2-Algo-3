f90ece70651a492d407655347712192154eda0d8
perti <aperticaro@fi.uba.ar>
2021-02-07 15:52:38 -0300

    Se agregaron pruebas para el personaje

--- src/main/test/edu/fiuba/algo3/PersonajeTest.java
+++ src/main/test/edu/fiuba/algo3/PersonajeTest.java
@@ -8,6 +8,66 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PersonajeTest {
 
     @Test
+    public void personajeBajaElLapizSinMoverseYNoPintaLaPosicionActual() {
+        Personaje personaje = new Personaje();
+
+        personaje.bajarLapiz();
+
+        assertFalse(personaje.obtenerPosicion().estaPintado());
+    }
+
+    @Test
+    public void personajeSeMueveHaciaAbajoSinPintarYSeMueveHaciaAbajoPintando() {
+        Personaje personaje = new Personaje();
+        Posicion posicionVieja = personaje.obtenerPosicion();
+        Posicion posicionEsperada = new Posicion(0, -1);
+
+        personaje.moverAbajo();
+
+        assertEquals(personaje.obtenerPosicion(), posicionEsperada);
+        assertFalse(personaje.obtenerPosicion().estaPintado() && posicionVieja.estaPintado());
+
+        personaje.bajarLapiz();
+        personaje.moverAbajo();
+
+        assertTrue(personaje.obtenerPosicion().estaPintado());
+    }
+
+    @Test
+    public void personajeSeMueveHaciaLaDerechaSinPintarYSeMueveHaciaLaDerechaPintando() {
+        Personaje personaje = new Personaje();
+        Posicion posicionVieja = personaje.obtenerPosicion();
+        Posicion posicionEsperada = new Posicion(1, 0);
+
+        personaje.moverDerecha();
+
+        assertEquals(personaje.obtenerPosicion(), posicionEsperada);
+        assertFalse(personaje.obtenerPosicion().estaPintado() && posicionVieja.estaPintado());
+
+        personaje.bajarLapiz();
+        personaje.moverDerecha();
+
+        assertTrue(personaje.obtenerPosicion().estaPintado());
+    }
+
+    @Test
+    public void personajeSeMueveHaciaLaIzquierdaSinPintarYSeMueveHaciaLaIzquierdaPintando() {
+        Personaje personaje = new Personaje();
+        Posicion posicionVieja = personaje.obtenerPosicion();
+        Posicion posicionEsperada = new Posicion(-1, 0);
+
+        personaje.moverIzquierda();
+
+        assertEquals(personaje.obtenerPosicion(), posicionEsperada);
+        assertFalse(personaje.obtenerPosicion().estaPintado() && posicionVieja.estaPintado());
+
+        personaje.bajarLapiz();
+        personaje.moverIzquierda();
+
+        assertTrue(personaje.obtenerPosicion().estaPintado());
+    }
+
+    @Test
     public void personajeComienzaConLapizLevantadoNoPintaLaPosicionAlMoverse(){
         Personaje personaje = new Personaje();
         Posicion posicionVieja = personaje.obtenerPosicion();
