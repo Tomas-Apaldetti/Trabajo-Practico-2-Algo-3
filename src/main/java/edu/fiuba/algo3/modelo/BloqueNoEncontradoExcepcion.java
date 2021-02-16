4766f905abb1c8548c3477e418776a50405aa7f4
Tomas-L-Apal <tapaldetti@fi.uba.ar>
2021-02-16 20:38:00 -0300

    fix: se agrego BloqueNoEncontradoExcepcion

---
+++ src/main/java/edu/fiuba/algo3/modelo/BloqueNoEncontradoExcepcion.java
@@ -0,0 +1,8 @@
+package edu.fiuba.algo3.modelo;
+
+public class BloqueNoEncontradoExcepcion extends RuntimeException {
+    public BloqueNoEncontradoExcepcion(String mensaje)
+    {
+        super(mensaje);
+    }
+}
